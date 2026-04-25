93c0978fed3f4aab38ff146c9c5cd6f073c0c471
WIN-FV2AUQGPUUL\DefinitelyBak <Halyl555@gmail.com>
2026-04-25 19:45:26 +0300

    Реализация матрицы.

--- src/math/Matrix.hpp
+++ src/math/Matrix.hpp
@@ -1,3 +1,6 @@
+// Hello world!
+
+
 #pragma once
 
 #include <cassert>
@@ -11,30 +14,28 @@
 
 namespace Algorithms::Math
 {
+    template <typename T>
+    class Matrix;
+
+    template <typename M>
+    concept RealMatrix = requires { typename std::decay_t<M>::is_real_matrix; };
+
     template <typename M>
     concept MatrixLike = requires(const std::decay_t<M>& m, size_t i, size_t j) {
         typename std::decay_t<M>::value_type;
-
         { m.rows() } -> std::convertible_to<size_t>;
         { m.cols() } -> std::convertible_to<size_t>;
         { m(i, j) } -> std::convertible_to<typename std::decay_t<M>::value_type>;
     };
 
-    template <typename T>
-    class Matrix;
-
-    template <typename M>
-    concept RealMatrix =
-        std::same_as<std::decay_t<M>, Matrix<typename std::decay_t<M>::value_type>>;
-
-    template <RealMatrix MatrixType>
+    template <RealMatrix T>
     class Submatrix
     {
     public:
-        using value_type = typename MatrixType::value_type;
+        using is_real_matrix = std::true_type;
+        using value_type = typename T::value_type;
 
-        Submatrix(MatrixType& matrix, size_t row_start, size_t col_start, size_t row_count,
-            size_t col_count)
+        Submatrix(T& matrix, size_t row_start, size_t col_start, size_t row_count, size_t col_count)
             : _matrix(matrix),
               _row_start(row_start),
               _col_start(col_start),
@@ -45,21 +46,27 @@ namespace Algorithms::Math
             assert(_col_start + _col_count <= _matrix.cols());
         }
 
+        template <typename OtherT>
+        Submatrix(Submatrix<OtherT>& other, size_t row_start, size_t col_start, size_t row_count,
+            size_t col_count)
+            : _matrix(other._matrix),
+              _row_start(other._row_start + row_start),
+              _col_start(other._col_start + col_start),
+              _row_count(row_count),
+              _col_count(col_count)
+        {
+            assert(row_start + row_count <= other.rows());
+            assert(col_start + col_count <= other.cols());
+        }
+
         template <MatrixLike Other>
         Submatrix& operator=(const Other& other)
         {
-            if constexpr (std::same_as<std::decay_t<Other>, Submatrix>)
-                if (static_cast<const void*>(this) ==
-                    static_cast<const void*>(std::addressof(other)))
-                    return *this;
-
             assert(other.rows() == _row_count);
             assert(other.cols() == _col_count);
-
             for (size_t i = 0; i < _row_count; ++i)
                 for (size_t j = 0; j < _col_count; ++j)
-                    _matrix(_row_start + i, _col_start + j) = other(i, j);
-
+                    (*this)(i, j) = other(i, j);
             return *this;
         }
 
@@ -68,123 +75,159 @@ namespace Algorithms::Math
 
         const value_type& operator()(size_t i, size_t j) const
         {
-            assert(i < _row_count && j < _col_count);
             return _matrix(_row_start + i, _col_start + j);
         }
 
         value_type& operator()(size_t i, size_t j)
+            requires(!std::is_const_v<T>)
         {
-            assert(i < _row_count && j < _col_count);
             return _matrix(_row_start + i, _col_start + j);
         }
 
+        auto submatrix(size_t r_s, size_t c_s, size_t r_c, size_t c_c)
+        {
+            return Submatrix<T>(*this, r_s, c_s, r_c, c_c);
+        }
+
+        auto submatrix(size_t r_s, size_t c_s, size_t r_c, size_t c_c) const
+        {
+            return Submatrix<std::add_const_t<Matrix<T>>>(*this, r_s, c_s, r_c, c_c);
+        }
+
     private:
-        MatrixType& _matrix;
-        const size_t _row_start;
-        const size_t _col_start;
-        const size_t _row_count;
-        const size_t _col_count;
+        template <RealMatrix U>
+        friend class Submatrix;
+        T& _matrix;
+        const size_t _row_start, _col_start, _row_count, _col_count;
     };
 
     template <typename T>
     class Matrix
     {
     public:
+        using is_real_matrix = std::true_type;
         using value_type = T;
 
-        Matrix(size_t rows, size_t cols) : _rows(rows), _cols(cols), _data(rows * cols)
-        {
-            assert(_rows > 0);
-            assert(_cols > 0);
-        }
+        Matrix(size_t rows, size_t cols) : _rows(rows), _cols(cols), _data(rows * cols) {}
+
         Matrix(std::initializer_list<std::initializer_list<T>> init)
         {
             _rows = init.size();
             assert(_rows > 0);
-
             _cols = init.begin()->size();
             assert(_cols > 0);
-
             _data.reserve(_rows * _cols);
-
             for (const auto& row : init)
             {
                 assert(row.size() == _cols);
                 _data.insert(_data.end(), row.begin(), row.end());
             }
         }
+
         template <MatrixLike Other>
-        Matrix(const Other& other)
+        Matrix(const Other& other) : _rows(other.rows()), _cols(other.cols()), _data(_rows * _cols)
         {
-            _rows = other.rows();
-            _cols = other.cols();
-
-            _data.resize(_rows * _cols);
-
             for (size_t i = 0; i < _rows; ++i)
                 for (size_t j = 0; j < _cols; ++j)
-                    _data[i * _cols + j] = other(i, j);
+                    (*this)(i, j) = other(i, j);
+        }
+
+        size_t rows() const { return _rows; }
+        size_t cols() const { return _cols; }
+
+        value_type& operator()(size_t i, size_t j)
+            requires(!std::is_const_v<T>)
+        {
+            return _data[i * _cols + j];
+        }
+        const value_type& operator()(size_t i, size_t j) const { return _data[i * _cols + j]; }
+
+        auto submatrix(size_t r_s, size_t c_s, size_t r_c, size_t c_c)
+        {
+            return Submatrix<Matrix<T>>(*this, r_s, c_s, r_c, c_c);
+        }
+
+        auto submatrix(size_t r_s, size_t c_s, size_t r_c, size_t c_c) const
+        {
+            return Submatrix<std::add_const_t<Matrix<T>>>(*this, r_s, c_s, r_c, c_c);
+        }
+
+    private:
+        size_t _rows, _cols;
+        std::vector<T> _data;
+    };
+
+    template <typename T>
+    class BufferMatrix
+    {
+    public:
+        using is_real_matrix = std::true_type;
+        using value_type = T;
+
+        BufferMatrix(value_type* buffer, size_t rows, size_t cols)
+            : _buffer(buffer), _rows(rows), _cols(cols)
+        {
         }
 
         template <MatrixLike Other>
-        Matrix& operator=(const Other& other)
+        BufferMatrix& operator=(const Other& other)
         {
-            if constexpr (std::same_as<std::decay_t<Other>, Matrix>)
-                if (static_cast<const void*>(this) ==
-                    static_cast<const void*>(std::addressof(other)))
-                    return *this;
-
-            assert(other.rows() > 0);
-            assert(other.cols() > 0);
-
-            if (_rows != other.rows() || _cols != other.cols())
-            {
-                _rows = other.rows();
-                _cols = other.cols();
-                _data.resize(_rows * _cols);
-            }
-
+            assert(_rows == other.rows());
+            assert(_cols == other.cols());
             for (size_t i = 0; i < _rows; ++i)
                 for (size_t j = 0; j < _cols; ++j)
-                    _data[i * _cols + j] = other(i, j);
-
+                    (*this)(i, j) = other(i, j);
             return *this;
         }
 
         size_t rows() const { return _rows; }
         size_t cols() const { return _cols; }
 
-        const value_type& operator()(size_t i, size_t j) const
-        {
-            assert(i < _rows && j < _cols);
-            return _data[i * _cols + j];
-        }
-
         value_type& operator()(size_t i, size_t j)
+            requires(!std::is_const_v<T>)
         {
-            assert(i < _rows && j < _cols);
-            return _data[i * _cols + j];
+            return _buffer[i * _cols + j];
+        }
+        const value_type& operator()(size_t i, size_t j) const { return _buffer[i * _cols + j]; }
+
+        auto submatrix(size_t r_s, size_t c_s, size_t r_c, size_t c_c)
+        {
+            return Submatrix<BufferMatrix<T>>(*this, r_s, c_s, r_c, c_c);
         }
 
-        Submatrix<Matrix> submatrix(
-            size_t row_start, size_t col_start, size_t row_count, size_t col_count)
+        auto submatrix(size_t r_s, size_t c_s, size_t r_c, size_t c_c) const
         {
-            return Submatrix<Matrix>(*this, row_start, col_start, row_count, col_count);
-        }
-
-        const Submatrix<Matrix> submatrix(
-            size_t row_start, size_t col_start, size_t row_count, size_t col_count) const
-        {
-            return Submatrix<Matrix>(*this, row_start, col_start, row_count, col_count);
+            return Submatrix<std::add_const_t<Matrix<T>>>(*this, r_s, c_s, r_c, c_c);
         }
 
     private:
         size_t _rows;
         size_t _cols;
-        std::vector<value_type> _data;
+        value_type* _buffer;
     };
 
-    template <typename Op, MatrixLike Lhs, MatrixLike Rhs>
+    template <typename T>
+    class ScalarWrapper
+    {
+    public:
+        using is_scalar_expression = std::true_type;
+        using value_type = T;
+
+        ScalarWrapper(T val) : _val(val) {}
+
+        size_t rows() const { return 0; }
+        size_t cols() const { return 0; }
+        value_type operator()(size_t, size_t) const { return _val; }
+
+    private:
+        value_type _val;
+    };
+
+    template <typename T>
+    inline constexpr bool is_scalar_v =
+        requires { typename std::decay_t<T>::is_scalar_expression; };
+
+    template <typename Op, typename Lhs, typename Rhs>
     class MatrixBinaryOp
     {
         template <typename T>
@@ -195,26 +238,37 @@ namespace Algorithms::Math
         };
 
     public:
-        using value_type = typename std::remove_cvref_t<Lhs>::value_type;
-
         using LhsStorage = typename Storage<Lhs>::type;
         using RhsStorage = typename Storage<Rhs>::type;
+        using value_type = typename std::decay_t<Lhs>::value_type;
 
         MatrixBinaryOp(Lhs&& lhs, Rhs&& rhs)
             : _lhs(std::forward<Lhs>(lhs)), _rhs(std::forward<Rhs>(rhs))
         {
-            assert(_lhs.rows() == _rhs.rows());
-            assert(_lhs.cols() == _rhs.cols());
+            if constexpr (!is_scalar_v<Lhs> && !is_scalar_v<Rhs>)
+            {
+                assert(_lhs.rows() == _rhs.rows());
+                assert(_lhs.cols() == _rhs.cols());
+            }
         }
 
-        size_t rows() const { return _lhs.rows(); }
-        size_t cols() const { return _lhs.cols(); }
-
-        value_type operator()(size_t i, size_t j) const
+        size_t rows() const
         {
-            assert(i < rows() && j < cols());
-            return Op{}(_lhs(i, j), _rhs(i, j));
+            if constexpr (is_scalar_v<Lhs>)
+                return _rhs.rows();
+            else
+                return _lhs.rows();
         }
+
+        size_t cols() const
+        {
+            if constexpr (is_scalar_v<Lhs>)
+                return _rhs.cols();
+            else
+                return _lhs.cols();
+        }
+
+        value_type operator()(size_t i, size_t j) const { return Op{}(_lhs(i, j), _rhs(i, j)); }
 
         Matrix<value_type> eval() const { return Matrix<value_type>(*this); }
 
@@ -243,32 +297,44 @@ namespace Algorithms::Math
             std::forward<Lhs>(lhs), std::forward<Rhs>(rhs));
     }
 
-    template <typename T>
-    class ScalarWrapper
-    {
-    public:
-        using value_type = T;
-        T value;
-
-        size_t rows() const { return 0; }
-        size_t cols() const { return 0; }
-
-        T operator()(size_t, size_t) const { return value; }
-    };
-
     template <MatrixLike Lhs, typename Rhs>
-        requires std::same_as<typename std::decay_t<Lhs>::value_type, Rhs>
+        requires std::convertible_to<std::decay_t<Rhs>, typename std::decay_t<Lhs>::value_type>
     auto operator*(Lhs&& lhs, Rhs&& rhs)
     {
-        using T = typename std::remove_cvref_t<Lhs>::value_type;
-        return MatrixBinaryOp<std::multiplies<T>, Lhs&&, ScalarWrapper<Rhs>&&>(
-            std::forward<Lhs>(lhs), ScalarWrapper<Rhs>{rhs});
+        using T = typename std::decay_t<Lhs>::value_type;
+        using S = ScalarWrapper<T>;
+        return MatrixBinaryOp<std::multiplies<T>, Lhs&&, S>(
+            std::forward<Lhs>(lhs), S(static_cast<T>(rhs)));
     }
 
     template <typename Lhs, MatrixLike Rhs>
-        requires std::same_as<Lhs, typename std::decay_t<Rhs>::value_type>
+        requires std::convertible_to<std::decay_t<Lhs>, typename std::decay_t<Rhs>::value_type>
     auto operator*(Lhs&& lhs, Rhs&& rhs)
     {
-        return std::forward<Rhs>(rhs) * std::forward<Lhs>(lhs);
+        using T = typename std::decay_t<Rhs>::value_type;
+        using S = ScalarWrapper<T>;
+        return MatrixBinaryOp<std::multiplies<T>, S, Rhs&&>(
+            S(static_cast<T>(lhs)), std::forward<Rhs>(rhs));
+    }
+
+    template <MatrixLike Lhs, MatrixLike Rhs>
+        requires std::same_as<typename std::decay_t<Lhs>::value_type, typename std::decay_t<Rhs>::value_type>
+    auto operator*(Lhs&& lhs, Rhs&& rhs)
+    {
+        assert(lhs.cols() == rhs.rows());
+        using T = typename std::decay_t<Lhs>::value_type;
+        Matrix<T> result(lhs.rows(), rhs.cols());
+        
+        for (size_t i = 0; i < lhs.rows(); ++i)
+        {
+            for (size_t j = 0; j < rhs.cols(); ++j)
+            {
+                T sum = 0;
+                for (size_t k = 0; k < lhs.cols(); ++k)
+                    sum += lhs(i, k) * rhs(k, j);
+                result(i, j) = sum;
+            }
+        }
+        return result;
     }
 }  // namespace Algorithms::Math
--- tests/Matrix.cpp
+++ tests/Matrix.cpp
@@ -33,19 +33,31 @@ namespace Algorithms::Tests
 
     TEST(MatrixTests, multiplies)
     {
-        Math::Matrix<int> a({{1, 2}, {3, 4}});
-        Math::Matrix<int> c = a * 3;
+        Math::Matrix<double> a({{1.0, 2.0}, {3.0, 4.0}});
+        Math::Matrix<double> c = a * 3;
 
-        EXPECT_EQ(c(0, 0), 3);
-        EXPECT_EQ(c(0, 1), 6);
-        EXPECT_EQ(c(1, 0), 9);
-        EXPECT_EQ(c(1, 1), 12);
+        EXPECT_EQ(c(0, 0), 3.);
+        EXPECT_EQ(c(0, 1), 6.);
+        EXPECT_EQ(c(1, 0), 9.);
+        EXPECT_EQ(c(1, 1), 12.);
 
         c = 3 * a;
-        EXPECT_EQ(c(0, 0), 3);
-        EXPECT_EQ(c(0, 1), 6);
-        EXPECT_EQ(c(1, 0), 9);
-        EXPECT_EQ(c(1, 1), 12);
+        EXPECT_EQ(c(0, 0), 3.);
+        EXPECT_EQ(c(0, 1), 6.);
+        EXPECT_EQ(c(1, 0), 9.);
+        EXPECT_EQ(c(1, 1), 12.);
+    }
+
+    TEST(MatrixTests, ComplexExpression)
+    {
+        Math::Matrix<int> a({{1, 1}, {1, 1}});
+        Math::Matrix<int> b({{2, 2}, {2, 2}});
+        Math::Matrix<int> c({{3, 3}, {3, 3}});
+
+        Math::Matrix<int> res = (a + b) * 2 - c;
+
+        EXPECT_EQ(res(0, 0), 3);
+        EXPECT_EQ(res(1, 1), 3);
     }
 
     TEST(MatrixTests, Submatrix)
@@ -58,20 +70,192 @@ namespace Algorithms::Tests
         EXPECT_EQ(sub(1, 1), 5);
     }
 
-    TEST(MatrixTests, DifficultSubmatrix)
+    TEST(MatrixTests, SubmatrixAssignment)
     {
         Math::Matrix<int> a({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        Math::Matrix<int> b({{10, 10}, {10, 10}});
+
+        auto sub = a.submatrix(1, 1, 2, 2);
+        sub = b * 5;
+
+        EXPECT_EQ(a(1, 1), 50);
+        EXPECT_EQ(a(2, 2), 50);
+        EXPECT_EQ(a(0, 0), 1);
+    }
+
+    TEST(MatrixTests, NestedSubmatrix)
+    {
+        Math::Matrix<int> a({{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}});
+
+        auto s1 = a.submatrix(1, 1, 2, 3);
+        auto s2 = s1.submatrix(1, 1, 1, 2);
+
+        EXPECT_EQ(s2.rows(), 1);
+        EXPECT_EQ(s2.cols(), 2);
+        EXPECT_EQ(s2(0, 0), 11);
+        EXPECT_EQ(s2(0, 1), 12);
+    }
+
+    TEST(MatrixTests, MismatchedSizesDeathTest)
+    {
+        Math::Matrix<int> a(2, 2);
+        Math::Matrix<int> b(3, 3);
+
+#ifndef NDEBUG
+        EXPECT_DEATH({ auto res = a + b; }, "");
+#endif
+    }
+
+    TEST(BufferMatrixTests, RawMemoryInteraction)
+    {
+        int raw_data[4] = {1, 2, 3, 4};
+        Math::BufferMatrix<int> bm(raw_data, 2, 2);
+
+        bm(0, 1) = 10;
+        EXPECT_EQ(raw_data[1], 10);
+
+        raw_data[3] = 40;
+        EXPECT_EQ(bm(1, 1), 40);
+    }
+
+    TEST(BufferMatrixTests, AssignmentFromExpression)
+    {
+        Math::Matrix<int> a({{1, 1}, {1, 1}});
+        Math::Matrix<int> b({{2, 2}, {2, 2}});
+
+        int result_ptr[4] = {0};
+        Math::BufferMatrix<int> target(result_ptr, 2, 2);
+
+        target = (a + b) * 5;
+
+        EXPECT_EQ(result_ptr[0], 15);
+        EXPECT_EQ(result_ptr[3], 15);
+    }
+
+    TEST(BufferMatrixTests, VectorInterop)
+    {
+        std::vector<double> data = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0};
+        Math::BufferMatrix<double> bm(data.data(), 2, 3);
+
+        EXPECT_EQ(bm(1, 0), 4.0);
+
+        bm(1, 2) = 0.0;
+        EXPECT_EQ(data[5], 0.0);
+    }
+
+    TEST(BufferMatrixTests, SubmatrixOfBuffer)
+    {
+        int data[9] = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+        Math::BufferMatrix<int> bm(data, 3, 3);
+
+        auto sub = bm.submatrix(1, 1, 2, 2);
+
+        EXPECT_EQ(sub(0, 0), 5);
+        EXPECT_EQ(sub(1, 1), 9);
+
+        sub(0, 0) = 50;
+        EXPECT_EQ(data[4], 50);
+    }
+
+    TEST(BufferMatrixTests, CopyToMatrix)
+    {
+        int data[4] = {1, 2, 3, 4};
+        Math::BufferMatrix<int> bm(data, 2, 2);
+
+        Math::Matrix<int> m = bm;
+
+        EXPECT_EQ(m(1, 1), 4);
+
+        m(1, 1) = 99;
+        EXPECT_EQ(data[3], 4);
+    }
+
+    TEST(BufferMatrixTests, FlatteningTypeCheck)
+    {
+        int data[16];
+        Math::BufferMatrix<int> bm(data, 4, 4);
+
+        auto s1 = bm.submatrix(0, 0, 3, 3);
+        auto s2 = s1.submatrix(0, 0, 2, 2);
+
+        bool same_type = std::is_same_v<decltype(s1), decltype(s2)>;
+        EXPECT_TRUE(same_type);
+    }
+
+    TEST(MatrixMultiplication, SquareMatrices)
+    {
+        Math::Matrix<int> a({{1, 2}, {3, 4}});
+        Math::Matrix<int> b({{5, 6}, {7, 8}});
+
+        // 1*5 + 2*7 = 19 | 1*6 + 2*8 = 22
+        // 3*5 + 4*7 = 43 | 3*6 + 4*8 = 50
+        Math::Matrix<int> c = a * b;
+
+        EXPECT_EQ(c.rows(), 2);
+        EXPECT_EQ(c.cols(), 2);
+        EXPECT_EQ(c(0, 0), 19);
+        EXPECT_EQ(c(0, 1), 22);
+        EXPECT_EQ(c(1, 0), 43);
+        EXPECT_EQ(c(1, 1), 50);
+    }
+
+    TEST(MatrixMultiplication, RectangularMatrices)
+    {
+        Math::Matrix<int> a({{1, 2, 3}, {4, 5, 6}});
+        Math::Matrix<int> b({{7, 8}, {9, 10}, {11, 12}});
+
+        Math::Matrix<int> c = a * b;
+
+        EXPECT_EQ(c.rows(), 2);
+        EXPECT_EQ(c.cols(), 2);
+        EXPECT_EQ(c(0, 0), 58);   // 1*7 + 2*9 + 3*11 = 7+18+33 = 58
+        EXPECT_EQ(c(1, 1), 154);  // 4*8 + 5*10 + 6*12 = 32+50+72 = 154
+    }
+
+    TEST(MatrixMultiplication, IdentityMatrix)
+    {
+        Math::Matrix<int> a({{1, 2}, {3, 4}});
+        Math::Matrix<int> id({{1, 0}, {0, 1}});
+
+        Math::Matrix<int> c = a * id;
+
+        EXPECT_EQ(c(0, 0), 1);
+        EXPECT_EQ(c(0, 1), 2);
+        EXPECT_EQ(c(1, 0), 3);
+        EXPECT_EQ(c(1, 1), 4);
+    }
+
+    TEST(MatrixMultiplication, WithExpressions)
+    {
+        Math::Matrix<int> a({{1, 1}, {1, 1}});
+        Math::Matrix<int> b({{2, 2}, {2, 2}});
+        Math::Matrix<int> c({{1, 0}, {0, 1}});
+
+        Math::Matrix<int> res = (a + b) * c;
+
+        EXPECT_EQ(res(0, 0), 3);
+        EXPECT_EQ(res(1, 1), 3);
+    }
+
+    TEST(MatrixMultiplication, SubmatrixMultiplication)
+    {
+        Math::Matrix<int> a({{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        Math::Matrix<int> b({{1, 0}, {0, 1}});
+
         auto sub = a.submatrix(0, 0, 2, 2);
-        EXPECT_EQ(sub(0, 0), 1);
-        EXPECT_EQ(sub(0, 1), 2);
-        EXPECT_EQ(sub(1, 0), 4);
-        EXPECT_EQ(sub(1, 1), 5);
-        
-        Math::Matrix<int> b({{1, 2}, {4, 5}});
-        Math::Matrix<int> c = sub + b;
-        EXPECT_EQ(c(0, 0), 2);
-        EXPECT_EQ(c(0, 1), 4);
-        EXPECT_EQ(c(1, 0), 8);
-        EXPECT_EQ(c(1, 1), 10);
+        Math::Matrix<int> res = sub * b;
+
+        EXPECT_EQ(res(0, 0), 1);
+        EXPECT_EQ(res(1, 1), 5);
+    }
+
+    TEST(MatrixMultiplication, DimensionMismatch)
+    {
+        Math::Matrix<int> a(2, 3);
+        Math::Matrix<int> b(2, 3);
+
+#ifndef NDEBUG
+        EXPECT_DEATH({ auto res = a * b; }, "");
+#endif
     }
 }
